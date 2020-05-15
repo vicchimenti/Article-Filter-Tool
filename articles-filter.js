2ac9295f1394d573d574c32c5f35477c87478cf6
Victor Chimenti <chimenti@seattleu.edu>
2020-05-15 07:49:57 -0700

    update select box to type

--- articles-filter.js
+++ articles-filter.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-$(function () {
+{/* { $(function () {
 	$('input#id_search').quicksearch('div.knowledgeBaseItemWrapper', {
 		'delay': 200,
 		'selector': '.knowledgeBaseItem',
@@ -45,7 +45,7 @@ $(function () {
 </script>
 
 
-<script>
+<script> } */}
 /***
 *   @author Victor Chimenti, MSCS 2020
 *   @file filter-search.js
@@ -118,104 +118,104 @@ $(function () {
             });
 
 
-						$(function () {
-							$('input#id_search').quicksearch('div.knowledgeBaseItemWrapper', {
-								'delay': 200,
-								'selector': '.knowledgeBaseItem',
-								'stripeRows': ['odd', 'even'],
-								'noResults': '.noResultsToShow',
-								'bind': 'keyup click',
-								'minValLength': 16,
-								'prepareQuery': function (val) {
-									return new RegExp(val, "i");
-								},
-								'testQuery': function (query, txt, _row) {
-									return query.test(txt);
-								},
-								'show': function() {
-									$(this).removeClass('hideByTextbox');
-								},
-								'hide': function() {
-									$(this).addClass('hideByTextbox');
-								}
-							});			
-						});
+						// $(function () {
+						// 	$('input#id_search').quicksearch('div.knowledgeBaseItemWrapper', {
+						// 		'delay': 200,
+						// 		'selector': '.knowledgeBaseItem',
+						// 		'stripeRows': ['odd', 'even'],
+						// 		'noResults': '.noResultsToShow',
+						// 		'bind': 'keyup click',
+						// 		'minValLength': 16,
+						// 		'prepareQuery': function (val) {
+						// 			return new RegExp(val, "i");
+						// 		},
+						// 		'testQuery': function (query, txt, _row) {
+						// 			return query.test(txt);
+						// 		},
+						// 		'show': function() {
+						// 			$(this).removeClass('hideByTextbox');
+						// 		},
+						// 		'hide': function() {
+						// 			$(this).addClass('hideByTextbox');
+						// 		}
+						// 	});			
+						// });
 
 
 
-            //   ***   Term Filter   ***   //
-            $(function() {
-                // When the Multi-Select Checkbox Selector for Academic Terms Changes - Execute change function 
-                $('#SelectBox-ByTerm').change(function () {
-                    // initialize an array of keys to hold each check box selected
-                    let termKeys = [];
-                    termKeys[0] = -1;
-                    $('input[name=SelectBox-ByTerm]:checked').each(function(item) {
-                        termKeys[item] = $(this).val();
-                    });
-                    // If Search Key array has at least one valid value then Compare to the Each Content Item in term
-                    if (termKeys[0] != -1) {
-                        $('.term').filter(function(i,e) {
-                            let termValue = $(this).text();
-                            // set state to hidden for all items
-                            $(this).parents('.courseItemWrapper').addClass('hideByTerm');
-                            // Check to see if any Key is a match with the current Value
-                            for (let index = 0; index < termKeys.length; index++) {
-                                if (termValue.match(termKeys[index])) {
-                                    // make current item visible when we validate a match
-                                    $(this).parents('.courseItemWrapper').removeClass('hideByTerm');
-                                }
-                            }
-                        });
-                    // Else the Search Key is Null so Reset all Content Items to Visible
-                    } else {
-                        $('.courseItemWrapper').removeClass('hideByTerm');
-                    }
-                    // parse out unselected content items and limit display to user selected items
-                    parseItems.process();
-                });
-            });
-
-
-
-
-            //   ***   Year Filter   ***   //
-            $(function () {
-                // When the Multi-Select Checkbox Selector for Calendar Year Changes - Execute change function 
-                $('#SelectBox-ByYear').change(function () {
-                    // initialize an array of keys to hold each check box selected
-                    let yearKeys = [];
-                    yearKeys[0] = -1;
-                    $('input[name=SelectBox-ByYear]:checked').each(function (item) {
-                        yearKeys[item] = $(this).val();
-                    });
-                    // If Search Key array has at least one valid value then Compare to the Each Content Item in year
-                    if (yearKeys[0] != -1) {
-                        $('.year').filter(function (i, e) {
-                            let yearValue = $(this).text();
-                            // set state to hidden for all items
-                            $(this).parents('.courseItemWrapper').addClass('hideByYear');
-                            // Check to see if any Key is a match with the current Value
-                            for (let index = 0; index < yearKeys.length; index++) {
-                                if (yearValue.match(yearKeys[index])) {
-                                    // make current item visible when we validate a match
-                                    $(this).parents('.courseItemWrapper').removeClass('hideByYear');
-                                }
-                            }
-                        });
-                        // Else the Search Key is Null so Reset all Content Items to Visible
-                    } else {
-                        $('.courseItemWrapper').removeClass('hideByYear');
-                    }
-                    // parse out unselected content items and limit display to user selected items
-                    parseItems.process();
-                });
-            });
+            // //   ***   Term Filter   ***   //
+            // $(function() {
+            //     // When the Multi-Select Checkbox Selector for Academic Terms Changes - Execute change function 
+            //     $('#SelectBox-ByTerm').change(function () {
+            //         // initialize an array of keys to hold each check box selected
+            //         let termKeys = [];
+            //         termKeys[0] = -1;
+            //         $('input[name=SelectBox-ByTerm]:checked').each(function(item) {
+            //             termKeys[item] = $(this).val();
+            //         });
+            //         // If Search Key array has at least one valid value then Compare to the Each Content Item in term
+            //         if (termKeys[0] != -1) {
+            //             $('.term').filter(function(i,e) {
+            //                 let termValue = $(this).text();
+            //                 // set state to hidden for all items
+            //                 $(this).parents('.courseItemWrapper').addClass('hideByTerm');
+            //                 // Check to see if any Key is a match with the current Value
+            //                 for (let index = 0; index < termKeys.length; index++) {
+            //                     if (termValue.match(termKeys[index])) {
+            //                         // make current item visible when we validate a match
+            //                         $(this).parents('.courseItemWrapper').removeClass('hideByTerm');
+            //                     }
+            //                 }
+            //             });
+            //         // Else the Search Key is Null so Reset all Content Items to Visible
+            //         } else {
+            //             $('.courseItemWrapper').removeClass('hideByTerm');
+            //         }
+            //         // parse out unselected content items and limit display to user selected items
+            //         parseItems.process();
+            //     });
+            // });
 
 
 
 
+            // //   ***   Year Filter   ***   //
+            // $(function () {
+            //     // When the Multi-Select Checkbox Selector for Calendar Year Changes - Execute change function 
+            //     $('#SelectBox-ByYear').change(function () {
+            //         // initialize an array of keys to hold each check box selected
+            //         let yearKeys = [];
+            //         yearKeys[0] = -1;
+            //         $('input[name=SelectBox-ByYear]:checked').each(function (item) {
+            //             yearKeys[item] = $(this).val();
+            //         });
+            //         // If Search Key array has at least one valid value then Compare to the Each Content Item in year
+            //         if (yearKeys[0] != -1) {
+            //             $('.year').filter(function (i, e) {
+            //                 let yearValue = $(this).text();
+            //                 // set state to hidden for all items
+            //                 $(this).parents('.courseItemWrapper').addClass('hideByYear');
+            //                 // Check to see if any Key is a match with the current Value
+            //                 for (let index = 0; index < yearKeys.length; index++) {
+            //                     if (yearValue.match(yearKeys[index])) {
+            //                         // make current item visible when we validate a match
+            //                         $(this).parents('.courseItemWrapper').removeClass('hideByYear');
+            //                     }
+            //                 }
+            //             });
+            //             // Else the Search Key is Null so Reset all Content Items to Visible
+            //         } else {
+            //             $('.courseItemWrapper').removeClass('hideByYear');
+            //         }
+            //         // parse out unselected content items and limit display to user selected items
+            //         parseItems.process();
+            //     });
+            // });
 
+
+
+
+						
 
             //   ***   Type Filter   ***   //
             $(function () {
@@ -225,7 +225,7 @@ $(function () {
                     let typeKey = $(this).val();
                     // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
                     if (typeKey) {
-                        $('.courseType').filter(function (i, e) {
+                        $('.knowledgeBaseItemWrapper').filter(function (i, e) {
                             var typeValue = $(this).text();
                             // Check to see if the Key and Value are a Match
                             if (typeValue.match(typeKey)) {
@@ -242,6 +242,26 @@ $(function () {
                     parseItems.process();
                 });
 						});
+
+						// $(function () {
+						// 	$('#selectbox').quicksearch('div.knowledgeBaseItemWrapper', {
+						// 		'delay': 200,
+						// 		'selector': 'ul',
+						// 		'bind': 'change',
+						// 		'prepareQuery': function (val) {
+						// 			return new RegExp(val, "i");
+						// 		},
+						// 		'testQuery': function (query, txt, _row) {
+						// 			return query.test(txt);
+						// 		},
+						// 		'show': function() {
+						// 			$(this).removeClass('hideByDropdown');
+						// 		},
+						// 		'hide': function() {
+						// 			$(this).addClass('hideByDropdown');
+						// 		}
+						// 	});			
+						// });
 						
 
 
